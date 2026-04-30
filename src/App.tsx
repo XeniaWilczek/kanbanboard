a7434e7dc925343102bd336704ceccde7f380322
Xenia Wilczek <xenia.wilczek@gmx.de>
2026-04-30 16:02:29 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import { Button } from "@base-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Board from "./routes/board/Board";
+import Details from "./routes/details/Details";
+import Profile from "./routes/profile/Profile";
 
 function App() {
-  return <Button>Standard Button</Button>;
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+
+        element: <Board />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/board/:id",
+        element: <Details />,
+      },
+    ],
+    {
+      basename: "/kanbanboard",
+    },
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
