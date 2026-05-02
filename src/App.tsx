ff3f66304ef4bfccc4afc9749096508a528123e5
Xenia Wilczek <xenia.wilczek@gmx.de>
2026-05-02 19:49:32 +0200

    Update structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Board from "./routes/board/Board";
+import Boards from "./routes/boards/Boards";
 import Details from "./routes/details/Details";
 import Profile from "./routes/profile/Profile";
+import ErrorPage from "./routes/errorPage";
+import Root from "./routes/root/Root";
 
 function App() {
   const router = createBrowserRouter(
     [
       {
         path: "/",
-
-        element: <Board />,
-      },
-      {
-        path: "/profile",
-        element: <Profile />,
-      },
-      {
-        path: "/board/:id",
-        element: <Details />,
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            path: "boards", // Boards als Child von Root
+            children: [
+              {
+                index: true,
+                element: <Boards />,
+              },
+              {
+                path: "details/:id", // Details als Child von Boards
+                element: <Details />,
+              },
+            ],
+          },
+          {
+            path: "profile", // Profile als Child von Root
+            element: <Profile />,
+          },
+        ],
       },
     ],
     {
       basename: "/kanbanboard",
     },
   );
-
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}></RouterProvider>;
 }
-
 export default App;
